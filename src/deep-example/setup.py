351441d1facc90af48791f68c3702e4adf2e0057
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2020-09-08 15:12:53 +0200

    created setup.py

---
+++ src/deep-example/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name="deep",version="0.1",
+    description="Example package",
+    author="Sjoerd Gnodde",url="https://intodeep.ai",
+    license='MIT',author_email="sgnodde@hhdelfland.nl",install_requires=['numpy', 'pandas'])
